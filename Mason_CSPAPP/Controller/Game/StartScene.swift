cea94c057969e3beb69141313ba8d1345ff9218b
Mason Childers <31513862+Vaposlays@users.noreply.github.com>
2017-12-14 09:07:58 -0700

    added the sprites and sounds to my project

--- Mason_CSPAPP/Controller/Game/StartScene.swift
+++ Mason_CSPAPP/Controller/Game/StartScene.swift
@@ -2,7 +2,7 @@
 //  StartScene.swift
 //  Sample_CSP
 //
-//  Created by Cody Henrichsen on 11/19/17.
+//  Created by Mason Childers on 11/19/17.
 //  Copyright © 2017 CTEC. All rights reserved.
 //
 
@@ -27,7 +27,17 @@ public class StartScene : SKScene
     
     override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
     {
+        let currentTouch = touches.first
+        let touchLocation = currentTouch?.location(in: self)
+        let touchNode = self.atPoint(touchLocation!)
         
+        if (touchNode.name == "start")
+        {
+            let gameOverScene = GameScene(size: size)
+            gameOverScene.scaleMode = scaleMode
+            let transitionTpe = SKTransition.flipHorizontal(withDuration: 1.0)
+            view?.presentScene(gameOverScene, transition: transitionTpe)
+        }
         
     }
 }
--- Mason_CSPAPP/Controller/ProgrammingController.swift
+++ Mason_CSPAPP/Controller/ProgrammingController.swift
@@ -7,29 +7,21 @@
 //
 
 import UIKit
+import SpriteKit
 
-class ProgrammingController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+public class ProgrammingController: UIViewController {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        let gameScene = StartScene(size:view.bounds.size)
+        let gameView = view as! SKView
+        
+        gameView.showsFPS = true
+        gameView.showsNodeCount = true
+        gameView.ignoresSiblingOrder = true
+        
+        gameScene.scaleMode = .resizeFill
+        gameView.presentScene(gameScene)
     }
-    */
-
 }
